ce78e92e2c71fe6fc5a52413131685d40493a9f7
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2025-02-02 17:47:56 +0800

    change deleting project env var route to use database

--- app/routes/api/v1/project/environment/delete.py
+++ app/routes/api/v1/project/environment/delete.py
@@ -25,10 +25,11 @@ async def delete_project_env_variable(
         response.status_code = status.HTTP_404_NOT_FOUND
         return f"Could not find project {project_id}"
 
-    if project.env_vars.get(body.key) is not None:
+    cursor = main.connection.cursor()
+    if len(cursor.execute("SELECT id FROM environments WHERE id = ? AND key = ?", [project_id, body.key]).fetchall()) > 0:
         unset_key(os.path.join(project.project_path, ".env"), body.key)
-        project.env_vars.pop(body.key)
+        cursor.execute("DELETE FROM environments WHERE id = ? AND key = ?", [project_id, body.key])
+        main.connection.commit()
     return {
         "key": body.key,
-        "value": body.value,
     }
